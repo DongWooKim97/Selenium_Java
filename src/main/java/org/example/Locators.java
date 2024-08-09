6d947a4990af2fbe65dde3292cd35b422e425252
DongWooKim97 <nanaukim@naver.com>
2024-08-09 16:43:47 +0900

    36.HTML 속성을 이용하여 사용자 지정 XPath와 CSS Selector 로케이터 만들기

--- src/main/java/org/example/Locators.java
+++ src/main/java/org/example/Locators.java
@@ -12,9 +12,8 @@ public class Locators {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/kimdongwoo/chromedriver-mac-arm64/chromedriver");
         WebDriver driver = new ChromeDriver();
-        // 암묵적 딜레이 설정 - 3초
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-
+        // 암묵적 딜레이 설정 - 5초
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys("dongwoo");
         driver.findElement(By.name("inputPassword")).sendKeys("dongwoo");
@@ -24,20 +23,23 @@ public class Locators {
         driver.findElement(By.linkText("Forgot your password?")).click();
 
 
+        // 웹페이지 내 HTML의 유일한 속성이 없을 때 + 로케이터의 속성이 없을 때는 CSS locator or XPath를 사용해야한다
         // Css Selector
         // Class name -> tagname.classname ->  button.signInBtn -> p.error
         // Id -> tagname#id -> input#inputUsername
         // 만약 HTML에 클래스 name이나 id가 없으면? -> Tagname[attribute='value']
         // Input[placeholder='Username']
 
-
         //  XPath
-        // Example, Tagname[@attribute='value']
+        // Example, //Tagname[@attribute='value']
         // <input type="text" placeholder="Name"> -> 이러한 태그가 있다면 아래와 같이 사용해야한다.
-        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+//        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+//        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@rsa.com");
+        driver.findElement(By.xpath(("//input[@type='text'][1]"))).sendKeys("kingkingding");
+        driver.findElement(By.cssSelector(("input[type='text']:nth-child(3)"))).sendKeys("12312312312");
 
 
-        // driver.quit();
+        // driver.quit();f
         // driver.close();
     }
 }
